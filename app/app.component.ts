ef35be823e2ec4f3bbd352225c005249a9cd5391
Johannes Ferner <johannes.ferner@ebcont.com>
2016-03-04 14:14:01 +0100

    integrated header compoinent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,8 @@
-import  {Component} from "angular2/core";
+import {Component} from "angular2/core";
 import {RouteConfig} from "angular2/router";
-//import {FooterComponent} from "./core/footer.component";
-//import {HeaderComponent} from "./core/header.component";
-//import {PaginationComponent} from "./core/pagination.component";
+import {FooterComponent} from "./core/footer.component";
+import {HeaderComponent} from "./core/header.component";
+import {PaginationComponent} from "./core/pagination.component";
 import {WelcomeComponent} from "./core/welcome.component";
 import {AlbumsComponent} from "./music/albums.component";
 import {AlbumComponent} from "./music/album.component";
@@ -13,8 +13,10 @@ import {RegisterComponent} from "./user/register.component";
 @Component({
     selector: "my-app",
     template: `
+        <header-component></header-component>
         <h1>Yeah yeah yeah!</h1>
     `,
+    directives: [FooterComponent, HeaderComponent]
 })
 @RouteConfig([
     {name: "Home", path: "/", component: WelcomeComponent},
