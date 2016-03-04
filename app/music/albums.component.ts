e819ee2c642ba48f0a176907c272cad0a730ae5d
Johannes Ferner <johannes.ferner@ebcont.com>
2016-03-04 13:54:23 +0100

    stub components

---
+++ app/music/albums.component.ts
@@ -0,0 +1,8 @@
+import  {Component} from 'angular2/core';
+
+@Component({
+    selector: 'album',
+    template: `<h1>AlbumComponent</h1>
+    `,
+})
+export class AlbumComponent {}
---
+++ app/user/register.component.ts
@@ -0,0 +1,8 @@
+import  {Component} from 'angular2/core';
+
+@Component({
+    selector: 'registration',
+    template: `<h1>RegistrationComponent</h1>
+    `,
+})
+export class RegistrationComponent {}
