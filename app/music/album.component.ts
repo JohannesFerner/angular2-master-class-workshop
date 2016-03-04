77aa15a0d9a06d688ea7d46c05234349c3c0b98a
Johannes Ferner <johannes.ferner@ebcont.com>
2016-03-04 17:27:27 +0100

    album id is logged now

--- app/music/album.component.ts
+++ app/music/album.component.ts
@@ -4,8 +4,10 @@ import {RouteParams} from "angular2/router"
 @Component({
     selector: "album-component",
     template: `<h1>AlbumComponent</h1>
-    `,
-    providers: [RouteParams]
+    `
 })
 export class AlbumComponent {
+    constructor(private routeParams:RouteParams) {
+        console.log(this.routeParams.get('id'));
+    }
 }
